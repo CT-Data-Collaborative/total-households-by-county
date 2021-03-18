8736949b891f8b9eea5796e9922741f0b6ce896f
ilyankou <ilyankou@gmail.com>
2021-03-18 11:25:38 +0000

    Update ACS 2019 5y

--- scripts/Total-Households-by-County-processing.R
+++ scripts/Total-Households-by-County-processing.R
@@ -17,7 +17,7 @@ source('./scripts/acsHelpers.R')
 
 #Get state data
 geography=geo.make(state=09)
-yearlist=c(2010:2018)
+yearlist=c(2010:2019)
 span = 5
 col.names="pretty" 
 key="ed0e58d2538fb239f51e01643745e83f380582d7"
@@ -107,7 +107,7 @@ households <- households %>%
 
 write.table (
   households,
-  file.path(getwd(), "data", "total-households-county-2018.csv"),
+  file.path(getwd(), "data", "total-households-county-2019.csv"),
   sep = ",",
   row.names = F,
   na = "-6666"
